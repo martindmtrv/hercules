29793adf2787d9d182aa3696844bd4042d413164
medwardson <mnaidooe@uwaterloo.ca>
2020-10-10 14:47:45 -0400

    Routine details changes

--- screens/RoutineDetailsScreen.tsx
+++ screens/RoutineDetailsScreen.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { SafeAreaView, ScrollView, StyleSheet } from 'react-native';
 import { TouchableOpacity } from 'react-native';
+import { RootData } from '../App';
 import { Text, View } from '../components/Themed';
 
 
@@ -12,15 +13,19 @@ export default function RoutineDetailsScreen({ route, navigation  }: {route:any,
     <View style={styles.container}>
       <Text style={styles.title}> {route.params.name} Day</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <SafeAreaView style={styles.container}>
+      <RootData.Consumer>
+      {(root) => (
+        <SafeAreaView style={styles.container}>
         <ScrollView style={styles.scrollView}>
           {[0, 1,2,3,4,5,6,7,8,9,10, 11, 12, 13, 14].map(item => <TouchableOpacity style={styles.box} onPress={()=> console.log(item)}>
-            <Text key={item}>Bench</Text>
+      <Text key={item}>{root.routines.find((routine) => routine.id === route.params.id)}</Text>
             <Text>Sets: </Text>
             <Text>Reps: </Text>
             </TouchableOpacity>)}
         </ScrollView>
-    </SafeAreaView>
+        </SafeAreaView>
+      )}
+      </RootData.Consumer>
     </View>
   );
 }
