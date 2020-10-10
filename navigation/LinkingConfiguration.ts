95a5d9159919a638923fa780be6d18488ab0e899
medwardson <mnaidooe@uwaterloo.ca>
2020-10-10 14:15:54 -0400

    Start o spotify stuff

--- navigation/LinkingConfiguration.ts
+++ navigation/LinkingConfiguration.ts
@@ -8,7 +8,7 @@ export default {
         screens: {
           Home: {
             screens: {
-              HomeScreen: 'one',
+              HomeScreen: 'home',
             },
           },
           Routines: {
@@ -23,7 +23,7 @@ export default {
           },
           Settings: {
             screens: {
-              SettingsScreen: 'four',
+              SettingsScreen: 'settings',
             },
           },
         },
