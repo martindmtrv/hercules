61bc497b7b78247e7f15573d822e70129ef52771
Martin Dimitrov <martin3dimitrov@gmail.com>
2020-10-10 14:21:33 -0400

    added app data state and file system savings

--- data/schemas/AppState.ts
+++ data/schemas/AppState.ts
@@ -1,10 +1,38 @@
-import {RoutineState} from './RoutineState'
-class AppState{
-  //  currentRoutine : RoutineState
-  //  constructor(){
-    //    this.initializeDay(routineName);
-   // }
- //   initializeDay(routineName : String) : void {
- //       this.currentRoutine = new RoutineState(routineName);
- //   }
+import {RoutineState} from './RoutineState';
+
+import AsyncStorage from "@react-native-community/async-storage";
+
+export class AppState{
+  routines: RoutineState[] = [];
+  currentDay: string = "";
+
+  initialized: boolean;
+
+  constructor() {
+    this.initialized = false;
+    this.routines = [];
+    this.currentDay = "";
+  }
+
+  loadData(): Promise<boolean> {
+    return AsyncStorage.getItem('app-data').then(data =>{
+      const obj: DBFormat = JSON.parse(data || "{}");
+      this.routines = obj.routines || [];
+      this.currentDay = obj.currentDay || "";
+      return this.initialized = true;
+    }).catch(() => {
+      console.log('there was a problem');
+      return false;
+    });  
+  }
+
+  saveData(): void {
+    AsyncStorage.setItem('app-data', JSON.stringify(this), (err) => err && console.log(err));
+  }
+}
+
+export interface DBFormat {
+  routines: RoutineState[];
+  currentDay: string;
+
 }
