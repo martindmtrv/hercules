9a5bc1fba9314449e843a1f4bdb2c2eec523fe5f
medwardson <mnaidooe@uwaterloo.ca>
2020-10-10 14:40:26 -0400

    Making stack for routine

--- screens/RoutineScreen.tsx
+++ screens/RoutineScreen.tsx
@@ -3,21 +3,17 @@ import { SafeAreaView, ScrollView, StyleSheet } from 'react-native';
 import { TouchableOpacity } from 'react-native';
 import { Text, View } from '../components/Themed';
 
+import {EXERCISES} from '../data/ExercisesMetaData';
 
-export default function RoutineDetailsScreen({ route, navigation  }: {route:any, navigation: any}) {
-
-  const days = ["Chest", "Back", "Legs", "Arms", "Shoulders"];
-
+export default function RoutineScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}> {route.params.name} Day</Text>
+      <Text style={styles.title}>Your Routines</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <SafeAreaView style={styles.container}>
         <ScrollView style={styles.scrollView}>
           {[0, 1,2,3,4,5,6,7,8,9,10, 11, 12, 13, 14].map(item => <TouchableOpacity style={styles.box} onPress={()=> console.log(item)}>
-            <Text key={item}>Bench</Text>
-            <Text>Sets: </Text>
-            <Text>Reps: </Text>
+            <Text key={item}>{EXERCISES[item].exercise}</Text>
             </TouchableOpacity>)}
         </ScrollView>
     </SafeAreaView>
@@ -32,19 +28,17 @@ const styles = StyleSheet.create({
     justifyContent: 'flex-start',
   },
   box: {
+    minWidth: "80%",
     display: 'flex',
-    flexDirection: "column",
-    alignItems: "center",
-    height: 200,
-    justifyContent: "center",
+    justifyContent: "flex-end",
     borderRadius: 6,
     borderStyle: "solid",
     borderWidth: 2,
-    marginBottom: 25,
+    marginBottom: 200,
     borderColor: "red",
   },
   routineName: {
-    fontSize: 30,
+    alignItems: "center",
     width: 'fit-content',
   },
   title: {
@@ -58,7 +52,7 @@ const styles = StyleSheet.create({
     width: '80%',
   },
   scrollView: {
-    width: "80vw",
+    minWidth: 800,
     marginVertical: 20,
   },
   text: {
